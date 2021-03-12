92790cccc33384db77b14bda9f482bbe390d84d2
Spilkey <spilkey1234@gmail.com>
2021-03-12 10:22:15 -0500

    added /home route

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ app.use(express.static('public'));
 app.use(express.static('dist'));
 app.use('/processParticles.js', express.static('server/processParticles.js'));
 
-const routes = require('./routes');
+const homePageRouter = require('./routes/home');
+const gamePageRouter = require('./routes/index');
 const sockets = require('./server/socketHandle.js');
 
+var path = require('path');
+global.appRoot = path.resolve(__dirname);
+global.viewPath = appRoot + "/public/views";
+
 //  Connect all our routes to our application
-app.use('/', routes);
+app.use('/', gamePageRouter);
+app.use('/home', homePageRouter);
 
 app.set('port', 8000);
 server.listen(8000, function() {
--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,3 @@
 exports.index = function(request, response) {
-    response.sendFile('/public/index.html', {root: __dirname})
+    response.sendFile(appRoot + '/public/index.html');
 };
