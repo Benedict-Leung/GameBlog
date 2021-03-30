140595e547de03ae90cb3b1a92af6ed9f965caf1
Spilkey <spilkey1234@gmail.com>
2021-03-30 10:50:15 -0400

    added routes for login and signup

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,11 @@
 exports.home = function(request, response) {
-    response.sendFile(viewPath + "/frontpage.html");
+    response.render('frontpage');
+};
+
+exports.login = function(request, response) {
+    response.render('login');
+};
+
+exports.signup = function(request, response) {
+    response.render('createaccount');
 };
--- routes/home.js
+++ routes/home.js
@@ -4,5 +4,7 @@ var router = express.Router();
 var homeController = require('../controllers/home');
 
 router.get('/', homeController.home);
+router.get('/login', homeController.login);
+router.get('/signup', homeController.signup);
 
 module.exports = router;
