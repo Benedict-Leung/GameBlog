8f758fb5e76b166f6e640fbcdad247dd4b3be462
Spilkey <spilkey1234@gmail.com>
2021-04-18 18:52:40 -0400

    example usage of blog

--- routes/blog.js
+++ routes/blog.js
@@ -1,22 +1,36 @@
 var express = require('express');
+const Blog = require('../models/blog');
+const BlogModel = require('../models/blogModel');
 var router = express.Router();
 
 //var blogController = require('../controllers/blog');
 
 router.get('/', (request, response) => {
-    const posts = [{
-        title: 'New Game of the Year???',
-        date: new Date(),
-        description: 'Man, this is my favourite game of all time'
-    }]
-    response.render('blog', { posts: posts });
+
+    // example usage of model
+    const model = new BlogModel();
+    model.getAllBlogs().then((data) => {
+        console.log(data);
+
+        // can use this when creating a new blog in the /new route
+        // here can use data straight from db
+        const testBlog = new Blog('New Game of the Year???', new Date(), 'Man, this is my favourite game of all time');
+        const testJson = testBlog.toJson();
+        
+        // for getting all the posts from the db use the line below comment the other line out
+        // const posts = data;
+        const posts = [testJson];
+
+        response.render('blog', { posts: posts });
+    }); 
 })
 
 router.get('/new', (request, response) => {
     response.render('partials/newpost');
 })
 
-router.post('/', (request, response) => {
+// use the insertBlog() and Blog dataclass to create a new blog to insert
+router.post('/new', (request, response) => {
 
 })
 
